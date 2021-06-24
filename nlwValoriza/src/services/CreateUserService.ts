411d479846146ff236a12bd82bdfd76ea0738c74
Diego <diegoferreiradoamaral@gmail.com>
2021-06-23 23:55:51 -0300

    Aula 4 - Trabalhando com JWT

--- nlwValoriza/src/services/CreateUserService.ts
+++ nlwValoriza/src/services/CreateUserService.ts
@@ -1,15 +1,17 @@
 import { getCustomRepository } from "typeorm";
-import { UsersRepositories } from "../repositories/UsersRepositories"
+import { UsersRepositories } from "../repositories/UsersRepositories";
+import { hash } from "bcryptjs"
 
 interface IUserRequest {
     name: string;
     email: string;
-    admin?: boolean
+    admin?: boolean;
+    password: string;
 }
 
 class CreateUserService {
 
-    async execute({ name, email, admin } : IUserRequest) {
+    async execute({ name, email, admin = false, password } : IUserRequest) {
         const userRepository = getCustomRepository(UsersRepositories);
 
         if(!email) {
@@ -24,11 +26,14 @@ class CreateUserService {
             throw new Error("User already exists");
         }
 
+        const passwordHash = await hash(password, 8)
+
         const user = userRepository.create({
             name,
             email,
             admin,
-        })
+            password: passwordHash,
+        });
 
         await userRepository.save(user);
 
